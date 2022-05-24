b394e5cf42d27a0e8f04c9bc9893e7135edc23e2
ALexBitInJava <sasha.golovin.javarush@gmail.com>
2022-05-25 00:18:02 +0300

    Сокобан. Тестовое задание

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -4,13 +4,9 @@ public class Win {
     public static boolean check(int[][] board) {
         boolean result = false;
         for (int i = 0; i < board.length; i++) {
-            for (int k = 0; k < board[i].length; k++) {
-                if (board[i][k] != '1') {
-                    if (monoHorizontal(board, i) || monoVertical(board, i)) {
-                        result = true;
-                        break;
-                    }
-                }
+            if (board[i][i] == 1 && (monoHorizontal(board, i) || monoVertical(board, i))) {
+                result = true;
+                break;
             }
         }
         return result;
